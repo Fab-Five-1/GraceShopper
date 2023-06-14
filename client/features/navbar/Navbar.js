48072baefa55184de6575a777d3fa2f6a626721a
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-13 19:19:35 -0500

    Made it so guests can see the home page and go to cart

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -28,6 +28,8 @@ const Navbar = () => {
         ) : (
           <div>
             {/* The navbar will show these links before you log in */}
+            <Link to="/home">Home</Link>
+            <Link to="/cart">Cart🛒</Link>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
           </div>
