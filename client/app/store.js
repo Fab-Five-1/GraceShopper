1273b99d48905d072ee9c31cf0b0f9af267aa3dd
Ivan <ivandeleon@me.com>
2023-06-14 17:41:49 -0400

    finishedsign up form

--- client/app/store.js
+++ client/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '../features/auth/authSlice';
 import singleProductSlice from "../features/singleProduct/singleProductSlice";
+import AddUserSlice from '../features/users/AddUserSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     singleProduct: singleProductSlice,
+    addUser: AddUserSlice
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 
