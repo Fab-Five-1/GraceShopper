06b74c414b8a5de64a0a1bcea70cc2a5aa8d6a59
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-14 17:54:49 -0500

    New method is way way better and cleaner

--- server/api/cart.js
+++ server/api/cart.js
@@ -7,13 +7,16 @@ const Product = require("../db/models/Product");
 router.get("/", async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization);
-    const order = await user.getOrder();
+    const userId = user.dataValues.id;
+    const order = await Order.findAll({
+      where: { userId },
+    });
     const orderId = order[0].dataValues.id;
     const orderProducts = await OrderProduct.findAll({
       where: { orderId },
     });
     console.log("Prodcut", orderProducts);
-    console.log("HEY", order[0].dataValues.id);
+    console.log("HEY", order);
     res.send({ user, order });
   } catch (err) {
     next(err);
