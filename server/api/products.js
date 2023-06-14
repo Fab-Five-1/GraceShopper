304207f56a3610eeee95c437fad53c215a77a8b3
maberizk <michelle.aberizk@gmail.com>
2023-06-14 09:58:28 -0700

    productROUTES

--- server/api/products.js
+++ server/api/products.js
@@ -4,11 +4,12 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
     try {
-        const  products = await Product.findAll({
+
+        const products = await Product.findAll({
             // // explicitly select only the id and username fields - even though
             // // users' passwords are encrypted, it won't help if we just
             // // send everything to anyone who asks!
-            // attributes: ['id', 'username']
+            attributes: ['name', 'description']
         })
         res.json(products)
         // res.send("hi!")
@@ -21,12 +22,15 @@ router.get('/', async (req, res, next) => {
 router.get("/:productId", async (req, res) => {
     try {
         const productId = req.params.productId;
-        const product = await Product.findByPk(productId);
+        const product = await Product.findByPk(productId, {
+            attributes: ['name', 'description']
+        });
         if (!product) {
             return res.status(404).send("Product not found")
         }
+        return res.send(product)
     }
     catch (err) {
-        next(err)
+        console.log(err)
     }
 });
