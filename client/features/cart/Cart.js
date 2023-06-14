bab9c7553894bbf24dffdc65e640e69fbab7e1fc
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-13 19:19:35 -0500

    Made it so guests can see the home page and go to cart

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -6,16 +6,28 @@ import { Link } from "react-router-dom";
  * COMPONENT
  */
 const Cart = () => {
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const username = useSelector((state) => state.auth.me.username);
 
   return (
     <div>
-      <h1>YO {username}</h1>
-      <h3>Welcome to your cart</h3>
-      <h3>Ready to checkout?</h3>
-      <Link to="/checkout">
-        <button type="button">Checkout</button>
-      </Link>
+      {isLoggedIn ? (
+        <div>
+          <h1>YO {username}</h1>
+          <h3>Welcome to your cart</h3>
+          <h3>Ready to checkout?</h3>
+          <Link to="/checkout">
+            <button type="button">Checkout</button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/home">Home</Link>
+          <Link to="/cart">Cart🛒</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      )}
     </div>
   );
 };
