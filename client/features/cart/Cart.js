00083fbe629389fbce7940e4502614c28a4bd4cb
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-15 16:13:04 -0500

    Refracted the cart code to run better

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -1,46 +1,42 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchUsersCart, selectCart } from "./CartSlice";
+import { fetchUsersCart, selectCart, updateOrderProducts } from "./CartSlice";
 
-/**
- * COMPONENT
- */
 const Cart = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-  if (isLoggedIn) {
-    const dispatch = useDispatch();
-    const usersInfo = useSelector(selectCart);
-    const firstName = useSelector((state) => state.auth.me.firstName);
+  const dispatch = useDispatch();
+  const { products, orderProducts, firstName } = useSelector(selectCart);
 
-    useEffect(() => {
+  useEffect(() => {
+    if (isLoggedIn) {
       dispatch(fetchUsersCart());
-    }, []);
+    }
+  }, [dispatch, fetchUsersCart, isLoggedIn]);
 
-    const { user, orders, orderProducts, products } = usersInfo;
-
-    const orderProductsQuantity = orderProducts.map(
-      (orderProduct) => orderProduct.numberOfItems
+  const handleQuantityChange = (event, orderProductId) => {
+    const newQuantity = parseInt(event.target.value);
+    const newOrderProducts = orderProducts.map((orderProduct) =>
+      orderProduct.id === orderProductId
+        ? { ...orderProduct, numberOfItems: newQuantity }
+        : orderProduct
     );
+    dispatch(updateOrderProducts(newOrderProducts));
+  };
 
-    const handleQuantityChange = (event, orderProductId) => {
-      const newQuantity = parseInt(event.target.value);
-      console.log(newQuantity);
-      const newOrderProducts = orderProducts.map((orderProduct) => {
-        if (orderProduct.id === orderProductId) {
-          return {
-            ...orderProduct,
-            numberOfItems: newQuantity,
-          };
-        }
-        return orderProduct;
-      });
-      dispatch(updateOrderProducts(newOrderProducts));
-    };
+  const calculateTotal = () => {
+    return products.reduce((total, product) => {
+      const orderProduct = orderProducts.find(
+        (op) => op.id === product.orderProductId
+      );
+      const productTotal = (orderProduct.numberOfItems * product.price) / 100;
+      return total + productTotal;
+    }, 0);
+  };
 
-    let total = 0;
-    return (
-      <div>
+  return (
+    <div>
+      {isLoggedIn ? (
         <div>
           <h1>Welcome to your cart, {firstName}!</h1>
           <section style={{ border: "5px solid red" }}>
@@ -49,35 +45,34 @@ const Cart = () => {
                 <h2>Items:</h2>
                 <div>
                   {products.map((product) => {
+                    const orderProduct = orderProducts.find(
+                      (op) => op.id === product.orderProductId
+                    );
                     const productTotal =
-                      (orderProductsQuantity[product.orderProductId - 1] *
-                        product.price) /
-                      100;
-                    total += productTotal;
+                      (orderProduct.numberOfItems * product.price) / 100;
 
                     return (
                       <div key={product.id}>
                         <input
                           style={{ margin: "0px 5px", width: "25px" }}
                           type="number"
-                          defaultValue={
-                            orderProductsQuantity[product.orderProductId - 1]
-                          }
+                          value={orderProduct.numberOfItems}
                           onChange={(event) =>
-                            handleQuantityChange(event, product.orderProductId)
+                            handleQuantityChange(event, orderProduct.id)
                           }
                         />
                         <span style={{ marginRight: "5px" }}>
                           {product.name}
                         </span>
-                        <span
-                          style={{ marginRight: "5px" }}
-                        >{`$ ${productTotal}`}</span>
+                        <span style={{ marginRight: "5px" }}>
+                          ${productTotal}
+                        </span>
                         <Link to={`/products/${product.id}`}>
                           <img
                             src={product.imageUrl}
-                            width={"50px"}
+                            width="50px"
                             style={{ border: "3px solid black" }}
+                            alt={product.name}
                           />
                         </Link>
                       </div>
@@ -89,27 +84,25 @@ const Cart = () => {
               <h2>Cart is empty</h2>
             )}
           </section>
-          <h3>{`Total: $${total}`}</h3>
+          <h3>Total: ${calculateTotal()}</h3>
           <h3>Ready to checkout?</h3>
           <Link to="/checkout">
             <button type="button">Checkout</button>
           </Link>
         </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Hi, you're not signed in!</h1>
-        <h3>Would you like to login or sign up?</h3>
-        <Link to="/login">Login</Link>
-        <br />
-        <Link to="/signup">Sign Up</Link>
-        <h3>Or continue as a guest</h3>
-        <Link to="/guestcheckout">Continue</Link>
-      </div>
-    );
-  }
+      ) : (
+        <div>
+          <h1>Hi, you're not signed in!</h1>
+          <h3>Would you like to login or sign up?</h3>
+          <Link to="/login">Login</Link>
+          <br />
+          <Link to="/signup">Sign Up</Link>
+          <h3>Or continue as a guest</h3>
+          <Link to="/guestcheckout">Continue</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
