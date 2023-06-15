c999bdb4641abf5e6ba31db0a1da234c7212364b
maberizk <michelle.aberizk@gmail.com>
2023-06-15 12:00:32 -0700

    singleProduct routes

--- server/api/products.js
+++ server/api/products.js
@@ -1,26 +1,28 @@
 const router = require("express").Router();
+const User = require("../db/models/User");
 const {
-  models: { Product },
+    models: { Product },
 } = require("../db");
 module.exports = router;
 
 router.get("/", async (req, res, next) => {
-  try {
-    const products = await Product.findAll();
-    res.json(products);
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const products = await Product.findAll();
+        res.json(products);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-router.get("/:productId", async (req, res) => {
-  try {
-    const productId = req.params.productId;
-    const product = await Product.findByPk(productId);
-    if (!product) {
-      return res.status(404).send("Product not found");
+router.get("/:productId", async (req, res, next) => {
+    try {
+        const productId = req.params.productId;
+        const product = await Product.findByPk(productId);
+        if (!product) {
+            return res.status(404).send("Product not found");
+        }
+        res.send(product);
+    } catch (err) {
+        console.log(err);
     }
-  } catch (err) {
-    next(err);
-  }
 });
