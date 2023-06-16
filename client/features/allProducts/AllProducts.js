222d3e4ce6c1a286c922ee400f9571d986cf04be
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-15 19:55:22 -0500

    Made the basic react handling and button add to cart on the home page

--- client/features/allProducts/AllProducts.js
+++ client/features/allProducts/AllProducts.js
@@ -4,17 +4,23 @@ import { NavLink } from "react-router-dom";
 //import SingleProduct from "../singleProduct/SingleProduct";
 import { selectProducts } from "./allProductsSlice";
 import { fetchProductsAsync } from "./allProductsSlice";
+import { createOrder } from "../cart/CartSlice";
 
 const AllProducts = () => {
   const products = useSelector(selectProducts);
   const dispatch = useDispatch();
 
+  const userId = useSelector((state) => state.auth.me.id);
   const isAdmin = useSelector((state) => state.auth.me.isAdmin);
-
 
   useEffect(() => {
     dispatch(fetchProductsAsync());
   }, [dispatch]);
+
+  const handleCartCreate = async (userId, productId) => {
+    console.log(userId, productId);
+    dispatch(createOrder({ userId, productId }));
+  };
 
   if (isAdmin) {
     const renderProducts = () => {
@@ -22,19 +28,60 @@ const AllProducts = () => {
         <div className="product" key={`All Products ${product.id}`}>
           <NavLink to={`/products/${product.id}`} className="product">
             <div className="product">
-              <img className="productImg" src={product.imageUrl} alt={product.name} />
+              <img
+                className="productImg"
+                src={product.imageUrl}
+                alt={product.name}
+              />
 
               <div className="productInfo">
-                <h3>
-                  {product.name}
-                </h3>
-                <h3>
-                  ${product.price / 100}
-                </h3>
+                <h3>{product.name}</h3>
+                <h3>${product.price / 100}</h3>
               </div>
             </div>
           </NavLink>
-          <button>Delete Product</button><button>Edit Product</button>
+          <button onClick={() => handleCartCreate(userId, product.id)}>
+            Add to Cart
+          </button>
+          <button>Delete Product</button>
+          <button>Edit Product</button>
+        </div>
+      ));
+    };
+
+    return (
+      <div id="products" className="column">
+        {products && products.length ? (
+          renderProducts()
+        ) : (
+          <div>
+            {console.log(products)}
+            <p>No products found.</p>
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    const renderProducts = () => {
+      return products.map((product) => (
+        <div className="product" key={`All Products ${product.id}`}>
+          <NavLink to={`/products/${product.id}`} className="product">
+            <div className="product">
+              <img
+                className="productImg"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+
+              <div className="productInfo">
+                <h3>{product.name}</h3>
+                <h3>${product.price / 100}</h3>
+              </div>
+            </div>
+          </NavLink>
+          <button onClick={() => handleCartCreate(userId, product.id)}>
+            Add to Cart
+          </button>
         </div>
       ));
     };
@@ -52,44 +99,6 @@ const AllProducts = () => {
       </div>
     );
   }
-
-
-  else {
-    const renderProducts = () => {
-      return products.map((product) => (
-        <div className="product" key={`All Products ${product.id}`}>
-          <NavLink to={`/products/${product.id}`} className="product">
-            <div className="product">
-              <img className="productImg" src={product.imageUrl} alt={product.name} />
-
-              <div className="productInfo">
-                <h3>
-                  {product.name}
-                </h3>
-                <h3>
-                  ${product.price / 100}
-                </h3>
-              </div>
-            </div>
-          </NavLink>
-        </div>
-      ));
-    };
-
-    return (
-      <div id="products" className="column">
-        {products && products.length ? (
-          renderProducts()
-        ) : (
-          <div>
-            {console.log(products)}
-            <p>No products found.</p>
-          </div>
-        )}
-      </div>
-    );
-  };
-}
-
+};
 
 export default AllProducts;
