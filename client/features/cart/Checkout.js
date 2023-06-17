dfeac07bda43b5e433e55a2021161f3c90656646
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-17 13:18:06 -0500

    Made it display when you place an order that it went through and worked

--- client/features/cart/Checkout.js
+++ client/features/cart/Checkout.js
@@ -1,11 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchUsersCart, selectCart } from "./CartSlice";
 import { updateOrder } from "./CheckoutSlice";
-/**
- * COMPONENT
- */
+
 const Checkout = () => {
   let userId = useSelector((state) => state.auth.me.id);
   if (!userId) {
@@ -20,7 +18,6 @@ const Checkout = () => {
   }, [fetchUsersCart]);
 
   let orderTotal = [];
-
   let orderId;
 
   if (Array.isArray(orders)) {
@@ -31,13 +28,22 @@ const Checkout = () => {
     orderId = orders.id;
   }
 
+  const [purchaseSuccess, setPurchaseSuccess] = useState(false);
+  const [orderNumber, setOrderNumber] = useState("");
+
   const handlePurchase = (e) => {
     e.preventDefault();
 
     orderId = orderId[0];
 
-    dispatch(updateOrder({ orderId, fulfilled: true }));
-    // dispatch(fetchUsersCart(userId));
+    dispatch(updateOrder({ orderId, fulfilled: true }))
+      .then(() => {
+        setOrderNumber(orderId);
+        setPurchaseSuccess(true);
+      })
+      .catch((error) => {
+        console.log("Error updating order:", error);
+      });
   };
 
   return (
@@ -50,15 +56,20 @@ const Checkout = () => {
       </h3>
       <form>
         <div>
-          <label htmlFor="email"> Email </label>
+          <label htmlFor="email">Email</label>
           <input type="text" name="email" />
-          <label htmlFor="shipping"> Shipping Address </label>
+          <label htmlFor="shipping">Shipping Address</label>
           <input type="text" name="shipping" />
-          <label htmlFor="card"> Card Number </label>
+          <label htmlFor="card">Card Number</label>
           <input type="text" name="card" />
-          <label htmlFor="cvv"> CVV </label>
+          <label htmlFor="cvv">CVV</label>
           <input type="text" name="cvv" />
-          <button onClick={handlePurchase}> Purchase </button>
+          <button onClick={handlePurchase}>Purchase</button>
+          {purchaseSuccess && (
+            <p>
+              Success! Your order has been placed. Order number: {orderNumber}
+            </p>
+          )}
         </div>
       </form>
     </div>
