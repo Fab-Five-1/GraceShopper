6b68c96e155a91beb9abdc1d9d4a6e0833aaf20f
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-17 13:23:22 -0500

    I made it so if the fields aren't filled out no user will be created

--- client/features/users/AddUser.js
+++ client/features/users/AddUser.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addNewUser } from "./AddUserSlice";
 
@@ -9,11 +8,24 @@ export default function AddUser() {
   const [email, setEmail] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [formError, setFormError] = useState(""); // State to track form errors
 
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Check if any of the form fields are empty
+    if (
+      !username.trim() ||
+      !password.trim() ||
+      !email.trim() ||
+      !firstName.trim() ||
+      !lastName.trim()
+    ) {
+      setFormError("All fields are required."); // Set form error message
+      return; // Exit the function
+    }
 
     const newUser = {
       username: username,
@@ -22,27 +34,25 @@ export default function AddUser() {
       firstName: firstName,
       lastName: lastName,
     };
-    console.log("submit", newUser);
+
     try {
-      // reduxx land
       dispatch(addNewUser(newUser));
 
+      // Clear form fields
       setUserName("");
       setPassword("");
       setEmail("");
       setFirstName("");
       setLastName("");
+      setFormError(""); // Clear form error message
     } catch (error) {
       console.log(error);
     }
   };
 
-  console.log("value of user info ----->", {});
-
   return (
-    // <div>User</div>
     <div>
-      <h1> Fill me out to create a new user</h1>
+      <h1>Fill me out to create a new user</h1>
       <form onSubmit={handleSubmit}>
         <div id={"add-user-form"}>
           <label htmlFor="username">Username</label>
@@ -52,38 +62,37 @@ export default function AddUser() {
             value={username}
             onChange={(e) => setUserName(e.target.value)}
           />
-
-          <label htmlFor="password"> Password </label>
+          <label htmlFor="password">Password</label>
           <input
             type="text"
             name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-
-          <label htmlFor="email"> Email </label>
+          <label htmlFor="email">Email</label>
           <input
             type="text"
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-
-          <label htmlFor="firstName"> First Name </label>
+          <label htmlFor="firstName">First Name</label>
           <input
             type="text"
             name="firstName"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           />
-          <label htmlFor="lastName"> Last Name </label>
+          <label htmlFor="lastName">Last Name</label>
           <input
             type="text"
             name="lastName"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
           />
-          <button type="submit"> Create </button>
+          {formError && <p>{formError}</p>}{" "}
+          {/* Display form error message if present */}
+          <button type="submit">Create</button>
         </div>
       </form>
     </div>
