5a72562cec5a549791cffdd745bf9a084d938fb5
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-17 19:02:23 -0500

    Looks alot better and more professional

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -56,7 +56,7 @@ const Cart = () => {
   };
 
   return (
-    <div id="cart">
+    <div id="cartHolder">
       {firstName ? (
         <h1 style={{ borderBottom: "5px solid black" }}>
           Welcome to your cart, {firstName} 😎
@@ -66,92 +66,99 @@ const Cart = () => {
           Welcome to your cart 😎
         </h1>
       )}
-      <div id="loginCart">
-        <section id="items">
-          {orderProducts.length > 0 ? (
-            <div>
-              {products.map((product) => {
-                const orderProduct = orderProducts.find(
-                  (op) => op.productId === product.id
-                );
-                const productTotal =
-                  (orderProduct.numberOfItems * product.price) / 100;
+      <div id="cart">
+        <div id="loginCart">
+          <section
+            style={{
+              padding: "20px",
+            }}
+          >
+            {orderProducts.length > 0 ? (
+              <div className="items">
+                {products.map((product) => {
+                  const orderProduct = orderProducts.find(
+                    (op) => op.productId === product.id
+                  );
+                  const productTotal =
+                    (orderProduct.numberOfItems * product.price) / 100;
 
-                return (
-                  <div
-                    key={product.id}
-                    style={{
-                      display: "inline-block",
-                      borderBottom: "5px solid rgba(0, 0, 0, 0.53)",
-                      padding: "20px",
-                    }}
-                  >
-                    <input
+                  return (
+                    <div
+                      key={product.id}
                       style={{
-                        margin: "0px 5px",
-                        width: "30px",
-                        fontSize: "25px",
+                        borderBottom: "2px solid rgba(0, 0, 0, 0.53)",
+                        padding: "20px",
                       }}
-                      type="number"
-                      value={orderProduct.numberOfItems}
-                      min={1}
-                      onChange={(event) =>
-                        handleQuantityChange(event, orderProduct.id)
-                      }
-                    />
-                    <span style={{ margin: "15px", fontSize: "25px" }}>
-                      {product.name}
-                    </span>
-                    <Link
-                      style={{ marginRight: "7px" }}
-                      to={`/products/${product.id}`}
                     >
-                      <img
-                        src={product.imageUrl}
-                        width="70px"
-                        style={{ border: "4px solid black" }}
-                        alt={product.name}
+                      <Link
+                        style={{ marginRight: "7px" }}
+                        to={`/products/${product.id}`}
+                      >
+                        <img
+                          src={product.imageUrl}
+                          width="100px"
+                          style={{ border: "3px solid black" }}
+                          alt={product.name}
+                        />
+                      </Link>
+                      <span style={{ margin: "15px", fontSize: "35px" }}>
+                        {product.name}
+                      </span>
+                      <input
+                        style={{
+                          margin: "0px 5px",
+                          width: "40px",
+                          fontSize: "35px",
+                        }}
+                        type="number"
+                        value={orderProduct.numberOfItems}
+                        min={1}
+                        onChange={(event) =>
+                          handleQuantityChange(event, orderProduct.id)
+                        }
                       />
-                    </Link>
-                    <span style={{ margin: "15px", fontSize: "25px" }}>
-                      ${productTotal}
-                    </span>
-                    <button
-                      style={{ fontSize: "20px" }}
-                      type="button"
-                      onClick={() => handleDelete(orderProduct.id)}
-                    >
-                      x
-                    </button>
-                  </div>
-                );
-              })}
-            </div>
-          ) : (
-            <h2 style={{ margin: "100px" }}>Cart is empty</h2>
-          )}
-        </section>
-        <section id="checkoutCart">
-          <label style={{ marginTop: "7px" }}>ENTER PROMO CODE</label>
-          <input
-            style={{ marginTop: "15px", padding: "5px" }}
-            type="text"
-            placeholder="Promo Code"
-          ></input>
-          <button style={{ padding: "5px" }}>SUBMIT</button>
-          <h3 style={{ fontSize: "20px" }}>Discount: $0</h3>
-          <h3 style={{ fontSize: "20px" }}>Total: ${calculateTotal()}</h3>
-          <h3 style={{ fontSize: "25px" }}>Ready to checkout?</h3>
-          <Link to="/checkout">
-            <button
-              style={{ fontSize: "20px" }}
-              type="button"
-              onClick={() => handleCheckout(calculateTotal())}
-            >
-              Checkout
-            </button>
-          </Link>
-        </section>
+                      <span style={{ margin: "15px", fontSize: "30px" }}>
+                        Total: ${productTotal}
+                      </span>
+                      <button
+                        id="deleteBtn"
+                        type="button"
+                        onClick={() => handleDelete(orderProduct.id)}
+                      >
+                        x
+                      </button>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <h2 style={{ margin: "100px" }}>Cart is empty</h2>
+            )}
+          </section>
+          <section id="checkoutCart">
+            <label style={{ marginTop: "7px", fontSize: "20px" }}>
+              ENTER PROMO CODE
+            </label>
+            <input
+              style={{ marginTop: "15px", padding: "5px" }}
+              type="text"
+              placeholder="Promo Code"
+            ></input>
+            <button style={{ padding: "5px" }}>SUBMIT</button>
+            <h3 style={{ fontSize: "20px" }}>Discount: $0</h3>
+            <h3 style={{ fontSize: "20px" }}>Total: ${calculateTotal()}</h3>
+            <h3 style={{ fontSize: "25px" }}>Ready to checkout?</h3>
+            <Link to="/checkout">
+              <button
+                style={{ fontSize: "20px" }}
+                type="button"
+                onClick={() => handleCheckout(calculateTotal())}
+              >
+                Checkout
+              </button>
+            </Link>
+          </section>
+        </div>
       </div>
     </div>
   );
